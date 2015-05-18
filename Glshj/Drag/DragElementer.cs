dc7da686cc4a73b01e8be6c1986d627f226ccfd8
jasam-sheja <jasam0jasam0jasam@gmail.com>
2015-05-18 12:28:39 +0300

    Drag
    create shape between two others

---
+++ Glshj/Drag/DragElementer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using Glshj.Abstract;
+
+namespace Glshj.Drag
+{
+    public class DragElementer :Elementer
+    {
+        public Elementer source;
+        public Elementer destinaion;
+        public CountParam sourceCount;
+
+        public DragElementer(Elementer source, Elementer destinaion, CountParam sourceCount)
+        {
+            this.source = source;
+            this.destinaion = destinaion;
+            this.sourceCount = sourceCount;
+        }
+
+        public override short[] getData()
+        {
+            short[] firstdata = source.getData();
+            short[] seconddata = destinaion.getData();
+            short[] data = new short[firstdata.Length + seconddata.Length];
+
+            int i = 0, j = 0, idx = 0;
+            //data[idx++] = firstdata[i++];
+            //data[idx++] = (short)(seconddata[j++] + sourceCount.Count);
+            
+            for (; i < firstdata.Length && j < seconddata.Length; i++, j++)
+            {
+                data[idx++] = (short)(seconddata[j] + sourceCount.Count);
+                data[idx++] = firstdata[i];
+                
+            }
+            for (; i < firstdata.Length; i++)
+                data[idx++] = firstdata[i];
+            for (; j < seconddata.Length; j++)
+                data[idx++] = (short)(seconddata[j] + sourceCount.Count);
+            return data;
+        }
+    }
+}
---
+++ Glshj/Drag/DragVertexer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using Glshj.Abstract;
+
+namespace Glshj.Drag
+{
+    public class DragVertexer:Vertexer
+    {
+        public Vertexer source;
+        public Vertexer destinaion;
+
+        public DragVertexer(Vertexer source, Vertexer destinaion)
+        {
+            this.source = source;
+            this.destinaion = destinaion;
+        }
+
+        public override Vector4[] getData()
+        {
+            Vector4[] data = new Vector4[Count];
+            Vector4[] firstdata = source.getData();
+            Vector4[] seconddata = destinaion.getData();
+            firstdata.CopyTo(data, 0);
+            seconddata.CopyTo(data, firstdata.Length);
+            return data;
+        }
+
+        public override int Count
+        {
+            get { return source.Count + destinaion.Count; }
+        }
+    }
+}
