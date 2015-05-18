452c1c159d4cb201e46a97db85abcbea8c87d711
jasam-sheja <jasam0jasam0jasam@gmail.com>
2015-05-18 12:31:50 +0300

    test project

---
+++ TestGljsh/MainTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGljsh
+{
+    class MainTester
+    {
+
+        [STAThread]
+        public static void Main()
+        {
+            using (Test2 example = new Test2())
+            {
+                example.Run(30.0, 0.0);
+            }
+            //using (ClothTest example = new ClothTest())
+            //{
+            //    example.Run(60.0, 0.0);
+            //}
+            //using (Test1 example = new Test1())
+            //{
+            //    example.Run(30.0, 0.0);
+            //}
+            //using (cubetest example = new cubetest())
+            //{
+            //    example.Run(30.0, 0.0);
+            //}
+        }
+
+    }
+}
