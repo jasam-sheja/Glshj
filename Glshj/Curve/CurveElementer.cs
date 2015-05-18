1f71cd08aa250c6ec13cd3bc7e064e5caed01fb4
jasam-sheja <jasam0jasam0jasam@gmail.com>
2015-05-18 12:21:15 +0300

    Curve
    1D shape

---
+++ Glshj/Curve/CurveElementer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Glshj.Abstract;
+
+namespace Glshj.Curve
+{
+    public class CurveElementer:Elementer
+    {
+        public CurveParam param;
+
+        public CurveElementer(CurveParam param)
+        {
+            this.param = param;
+        }
+
+        public override short[] getData()
+        {
+            short[] res = new short[param.Count];
+            for (short i = 0; i < res.Length; i++)
+                res[i] = i;
+            return res;
+        }
+    }
+}
